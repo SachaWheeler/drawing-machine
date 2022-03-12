1b6aa5f512e6b52af41610f40c6b2b2196272bf5
SachaWheeler <sacha@sachawheeler.com>
2022-03-12 10:39:41 +0000

    working motor drivers and LCD screen

---
+++ drawing_machine.ino
@@ -0,0 +1,91 @@
+// include the lcd code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6; // 2, 3, 4, 5 become 6, 7, 8, 9
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// motors
+int Arm1Pin = 2;
+int PlatterPin = 3;
+int Arm2Pin = 4;
+
+void setup() {
+  // LCD
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+
+  // motors
+  pinMode(Arm1Pin, OUTPUT);
+  pinMode(Arm2Pin, OUTPUT);
+  pinMode(PlatterPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+
+}
+
+void loop() {
+  if (Serial.available()) {
+      int speed = Serial.parseInt();
+      if (speed >= 0 && speed <= 255) {
+         analogWrite(Arm1Pin, speed);
+         delay(1000);
+         analogWrite(Arm2Pin, speed);
+         delay(1000);
+         analogWrite(PlatterPin, speed);
+      }
+   }
+
+  // LCD
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
+}
+
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
+
+*/
