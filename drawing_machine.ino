6dec9effb65be4c9c9b87b91fb399025d166a69c
SachaWheeler <sacha@sachawheeler.com>
2022-03-16 20:26:00 +0000

    motor controls and start / stop working

--- drawing_machine.ino
+++ drawing_machine.ino
@@ -11,7 +11,7 @@ const int PlatterPin = 3;
 const int Arm2Pin = 4;
 
 // control panel
-const int START = 22;     // the number of the pushbutton pin
+const int START_BUTTON = 22;     // the number of the pushbutton pin
 const int BUTTON_1 = 36;   // row 1 col 1
 const int BUTTON_2 = 38;   // row 2 col 1
 const int BUTTON_3 = 40;   // row 3 col 1
@@ -31,7 +31,7 @@ const int max_arm1_speed = 255;
 const int max_arm2_speed = 255;
 
 bool motorsOn = false;
-int start;
+int start_button;
 
 int button_1_state = 0;
 int button_2_state = 0;
@@ -47,7 +47,12 @@ String status;
 
 unsigned long startMillis;
 unsigned long currentMillis;
-const unsigned long period = 100;  //the value is a number of milliseconds
+const unsigned long period = 100;  // milliseconds
+
+const bool ON = LOW;
+const bool OFF = HIGH;
+
+const bool RUN_MOTORS = true; // change this to false for testing without motors
 
 void setup() {
   // LCD
@@ -58,16 +63,16 @@ void setup() {
   lcd.print(status);
 
   // buttons
-  pinMode(START, INPUT);
-  pinMode(BUTTON_1, INPUT);
-  pinMode(BUTTON_2, INPUT);
-  pinMode(BUTTON_3, INPUT);
-  pinMode(BUTTON_4, INPUT);
-  pinMode(BUTTON_5, INPUT);
-  pinMode(BUTTON_6, INPUT);
-  pinMode(BUTTON_7, INPUT);
-  pinMode(BUTTON_8, INPUT);
-  pinMode(BUTTON_9, INPUT);
+  pinMode(START_BUTTON, INPUT_PULLUP);
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  pinMode(BUTTON_3, INPUT_PULLUP);
+  pinMode(BUTTON_4, INPUT_PULLUP);
+  pinMode(BUTTON_5, INPUT_PULLUP);
+  pinMode(BUTTON_6, INPUT_PULLUP);
+  pinMode(BUTTON_7, INPUT_PULLUP);
+  pinMode(BUTTON_8, INPUT_PULLUP);
+  pinMode(BUTTON_9, INPUT_PULLUP);
 
   // motors
   pinMode(Arm1Pin, OUTPUT);
@@ -84,38 +89,42 @@ void setup() {
 
 void loop() {
 
-  start = digitalRead(START);
+  start_button = digitalRead(START_BUTTON);
 
   // Serial.print("start: ");
   // Serial.println(start);
-  if (start == HIGH && motorsOn == false) { // run the motors
+  if (start_button == ON && motorsOn == false) { // run the motors
     Serial.println("run");
 
-    // start them
-    // analogWrite(Arm1Pin, Arm1Speed);
-    // analogWrite(Arm2Pin, Arm2Speed);
-    // analogWrite(PlatterPin, PlatterSpeed);
+    if (RUN_MOTORS == true) { // start them
+      analogWrite(Arm1Pin, Arm1Speed);
+      analogWrite(Arm2Pin, Arm2Speed);
+      analogWrite(PlatterPin, PlatterSpeed);
+      Serial.println("motors started");
+    }
     motorsOn = true;
-    Serial.println("motors started");
+
 
   }
 
-  if (start == LOW) {
-    Serial.println("stop");
+  if (start_button == OFF) {
     if (motorsOn == true) { // stop the motors
-      // analogWrite(PlatterPin, 0);
-      // analogWrite(Arm1Pin, 0);
-      // analogWrite(Arm2Pin, 0);
+      Serial.println("stop");
+      if (RUN_MOTORS == true) {
+        analogWrite(PlatterPin, 0);
+        analogWrite(Arm1Pin, 0);
+        analogWrite(Arm2Pin, 0);
+        Serial.println("motors stopped");
+      }
       motorsOn = false;
-      Serial.println("motors stopped");
     }
     // set the speeds
     button_1_state = digitalRead(BUTTON_1);
     button_2_state = digitalRead(BUTTON_2);
     button_3_state = digitalRead(BUTTON_3);
     button_4_state = digitalRead(BUTTON_4);
-    button_5_state = digitalRead(BUTTON_5);
-    button_6_state = digitalRead(BUTTON_6);
+    button_5_state = digitalRead(BUTTON_6); // changed from BUTTON_5 due to wiring issue
+    button_6_state = digitalRead(BUTTON_6); // currently serving BUTTON_5 data
     button_7_state = digitalRead(BUTTON_7);
     button_8_state = digitalRead(BUTTON_8);
     button_9_state = digitalRead(BUTTON_9);
@@ -126,46 +135,39 @@ void loop() {
     //Serial.print(button_5_state);
     //Serial.print(button_7_state);
     //Serial.println(button_8_state);
-    String buttons = String(start) + ":" + String(button_1_state) + " " + String(button_2_state) + " " + String(button_4_state) + " " + String(button_5_state) + " " + String(button_7_state) + " " + String(button_8_state);
+    // String buttons = String(start) + ":" + String(button_1_state) + " " + String(button_2_state) + " " + String(button_4_state) + " " + String(button_5_state) + " " + String(button_7_state) + " " + String(button_8_state);
+    //    Serial.println(buttons);
 
-    if (button_1_state == HIGH && PlatterSpeed < max_platter_speed){ // platter speed up
+    if (button_1_state == ON && PlatterSpeed < max_platter_speed) { // platter speed up
       PlatterSpeed += 1;
-      button_1_state = LOW;
+      Serial.println("button 1");
     }
-    if (button_2_state == HIGH && PlatterSpeed > 0){ // platter speed down
+    if (button_2_state == ON && PlatterSpeed > 0) { // platter speed down
       PlatterSpeed -= 1;
-      button_2_state = LOW;
+      Serial.println("button 2");
     }
 
-    if (button_4_state == HIGH && Arm1Speed < max_arm1_speed){ // Arm1 speed up
+    if (button_4_state == ON && Arm1Speed < max_arm1_speed) { // Arm1 speed up
       Arm1Speed += 1;
-      button_4_state = LOW;
+      Serial.println("button 4");
     }
-    if (button_5_state == HIGH && Arm1Speed > 0){ // Arm1Speed speed down
+    if (button_5_state == ON && Arm1Speed > 0) { // Arm1Speed speed down
       Arm1Speed -= 1;
-      button_5_state = LOW;
+      Serial.println("button 5");
     }
 
-    if (button_7_state == HIGH && Arm2Speed < max_arm2_speed){ // Arm2 speed up
+    if (button_7_state == ON && Arm2Speed < max_arm2_speed) { // Arm2 speed up
       Arm2Speed += 1;
-      button_7_state = LOW;
+      Serial.println("button 7");
     }
-    if (button_8_state == HIGH && Arm2Speed > 0){ // Arm2 speed down
+    if (button_8_state == ON && Arm2Speed > 0) { // Arm2 speed down
       Arm2Speed -= 1;
-      button_8_state = LOW;
+      Serial.println("button 8");
     }
 
     status = "P:" + String(PlatterSpeed) + ",A:" + Arm1Speed + ",B:" + Arm2Speed + "   ";
     lcd.setCursor(0, 0);
     lcd.print(status);
-    lcd.setCursor(0, 1);
-    lcd.print(buttons);
   }
+  // delay(100);
 }
-
-// LCD
-// set the cursor to column 0, line 1
-// (note: line 1 is the second row, since counting begins with 0):
-//lcd.setCursor(0, 1);
-// print the number of seconds since reset:
-// lcd.print(millis() / 1000);
