5057015ada8ed9cba2bed6a707952a5418c4e1b1
SachaWheeler <sacha@sachawheeler.com>
2022-03-16 20:42:50 +0000

    correct the arm controls and clean up the code

--- drawing_machine.ino
+++ drawing_machine.ino
@@ -6,12 +6,12 @@ const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6; // 2, 3, 4, 5 become
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // motors
-const int Arm1Pin = 2;
+const int Arm2Pin = 2;
 const int PlatterPin = 3;
-const int Arm2Pin = 4;
+const int Arm1Pin = 4;
 
 // control panel
-const int START_BUTTON = 22;     // the number of the pushbutton pin
+const int START_BUTTON = 22; // the start button
 const int BUTTON_1 = 36;   // row 1 col 1
 const int BUTTON_2 = 38;   // row 2 col 1
 const int BUTTON_3 = 40;   // row 3 col 1
@@ -23,12 +23,13 @@ const int BUTTON_8 = 50;   // row 2 col 3
 const int BUTTON_9 = 52;   // row 3 col 3
 
 // variables
-int PlatterSpeed = 100; // initial variables, will change
+int PlatterSpeed = 60; // initial variables, will change
 int Arm1Speed = 100;
 int Arm2Speed = 100;
 const int max_platter_speed = 255;
 const int max_arm1_speed = 255;
 const int max_arm2_speed = 255;
+const int motor_min = 50;
 
 bool motorsOn = false;
 int start_button;
@@ -43,11 +44,15 @@ int button_7_state = 0;
 int button_8_state = 0;
 int button_9_state = 0;
 
-String status;
+bool button_change = false;
 
+String lcd_status;
+
+/*
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 100;  // milliseconds
+*/
 
 const bool ON = LOW;
 const bool OFF = HIGH;
@@ -58,9 +63,9 @@ void setup() {
   // LCD
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  status = "P:" + String(PlatterSpeed) + ",A:" + Arm1Speed + ",B:" + Arm2Speed;
+  lcd_status = "P:" + String(PlatterSpeed) + ",A:" + Arm1Speed + ",B:" + Arm2Speed;
   lcd.setCursor(0, 0);
-  lcd.print(status);
+  lcd.print(lcd_status);
 
   // buttons
   pinMode(START_BUTTON, INPUT_PULLUP);
@@ -80,19 +85,13 @@ void setup() {
   pinMode(PlatterPin, OUTPUT);
   Serial.begin(9600);
 
-  startMillis = millis();  //initial start time
-
-  // serial input deprecated
-  // while (! Serial);
-  // Serial.println("Speed 0 to 255");
+  // startMillis = millis();  //initial start time
 }
 
 void loop() {
 
   start_button = digitalRead(START_BUTTON);
 
-  // Serial.print("start: ");
-  // Serial.println(start);
   if (start_button == ON && motorsOn == false) { // run the motors
     Serial.println("run");
 
@@ -103,8 +102,6 @@ void loop() {
       Serial.println("motors started");
     }
     motorsOn = true;
-
-
   }
 
   if (start_button == OFF) {
@@ -128,46 +125,40 @@ void loop() {
     button_7_state = digitalRead(BUTTON_7);
     button_8_state = digitalRead(BUTTON_8);
     button_9_state = digitalRead(BUTTON_9);
-    //Serial.print("buttons:");
-    //Serial.print(button_1_state);
-    //Serial.print(button_2_state);
-    //Serial.print(button_4_state);
-    //Serial.print(button_5_state);
-    //Serial.print(button_7_state);
-    //Serial.println(button_8_state);
-    // String buttons = String(start) + ":" + String(button_1_state) + " " + String(button_2_state) + " " + String(button_4_state) + " " + String(button_5_state) + " " + String(button_7_state) + " " + String(button_8_state);
-    //    Serial.println(buttons);
 
     if (button_1_state == ON && PlatterSpeed < max_platter_speed) { // platter speed up
       PlatterSpeed += 1;
-      Serial.println("button 1");
+      button_change = true;
     }
-    if (button_2_state == ON && PlatterSpeed > 0) { // platter speed down
+    if (button_2_state == ON && PlatterSpeed > motor_min) { // platter speed down
       PlatterSpeed -= 1;
-      Serial.println("button 2");
+      button_change = true;
     }
 
     if (button_4_state == ON && Arm1Speed < max_arm1_speed) { // Arm1 speed up
       Arm1Speed += 1;
-      Serial.println("button 4");
+      button_change = true;
     }
-    if (button_5_state == ON && Arm1Speed > 0) { // Arm1Speed speed down
+    if (button_5_state == ON && Arm1Speed > motor_min) { // Arm1Speed speed down
       Arm1Speed -= 1;
-      Serial.println("button 5");
+      button_change = true;
     }
 
     if (button_7_state == ON && Arm2Speed < max_arm2_speed) { // Arm2 speed up
       Arm2Speed += 1;
-      Serial.println("button 7");
+      button_change = true;
     }
-    if (button_8_state == ON && Arm2Speed > 0) { // Arm2 speed down
+    if (button_8_state == ON && Arm2Speed > motor_min) { // Arm2 speed down
       Arm2Speed -= 1;
-      Serial.println("button 8");
+      button_change = true;
     }
 
-    status = "P:" + String(PlatterSpeed) + ",A:" + Arm1Speed + ",B:" + Arm2Speed + "   ";
-    lcd.setCursor(0, 0);
-    lcd.print(status);
+    if (button_change == true) {
+      lcd_status = "P:" + String(PlatterSpeed) + " A:" + Arm1Speed + ",B:" + Arm2Speed + "   ";
+      lcd.setCursor(0, 0);
+      lcd.print(lcd_status);
+      delay(100);
+      button_change = false;
+    }
   }
-  // delay(100);
 }
