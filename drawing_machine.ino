b5ea161e87764ae2b9553eb9184bfca4c375d77b
SachaWheeler <sacha@sachawheeler.com>
2022-03-17 14:45:07 +0000

    clean up code, introduce wave form functions

--- drawing_machine.ino
+++ drawing_machine.ino
@@ -6,65 +6,92 @@ const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6; // 2, 3, 4, 5 become
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // motors
-const int Arm2Pin = 2;
+const int Arm2Pin    = 2;
 const int PlatterPin = 3;
-const int Arm1Pin = 4;
+const int Arm1Pin    = 4;
 
 // control panel
-const int START_BUTTON = 22; // the start button
-const int BUTTON_1 = 36;   // row 1 col 1
-const int BUTTON_2 = 38;   // row 2 col 1
-const int BUTTON_3 = 40;   // row 3 col 1
-const int BUTTON_4 = 42;   // row 1 col 2
-const int BUTTON_5 = 44;   // row 2 col 2
-const int BUTTON_6 = 46;   // row 3 col 2
-const int BUTTON_7 = 48;   // row 1 col 3
-const int BUTTON_8 = 50;   // row 2 col 3
-const int BUTTON_9 = 52;   // row 3 col 3
+const int START_BUTTON = 22; // start button
+const int BUTTON_1 = 36;     // row 1 col 1
+const int BUTTON_2 = 38;     // row 2 col 1
+const int BUTTON_3 = 40;     // row 3 col 1
+const int BUTTON_4 = 42;     // row 1 col 2
+const int BUTTON_5 = 44;     // row 2 col 2
+const int BUTTON_6 = 46;     // row 3 col 2
+const int BUTTON_7 = 48;     // row 1 col 3
+const int BUTTON_8 = 50;     // row 2 col 3
+const int BUTTON_9 = 52;     // row 3 col 3
 
 // variables
 int PlatterSpeed = 60; // initial variables, will change
-int Arm1Speed = 100;
-int Arm2Speed = 100;
+int Arm1Speed    = 150;
+int Arm2Speed    = 150;
+
 const int MAX_PLATTER_SPEED = 255;
-const int MAX_ARM1_SPEED = 255;
-const int MAX_ARM2_SPEED = 255;
-const int MOTOR_MIN = 55;
+const int MAX_ARM1_SPEED    = 255;
+const int MAX_ARM2_SPEED    = 255;
+const int MOTOR_MIN         = 55;
 
 bool motorsOn = false;
 int start_button;
 
-int button_1_state = 0;
-int button_2_state = 0;
-int button_3_state = 0;
-int button_4_state = 0;
-int button_5_state = 0;
-int button_6_state = 0;
-int button_7_state = 0;
-int button_8_state = 0;
-int button_9_state = 0;
-
+int button_1_state = 0, button_2_state = 0, button_3_state = 0, button_4_state = 0, button_5_state = 0, button_6_state = 0, button_7_state = 0, button_8_state = 0, button_9_state = 0;
 bool button_change = false;
+
+int arm1_wave = 0, arm2_wave = 0, arm1_period = 1, arm2_period = 1, arm1_amp = 1, arm2_amp = 1;
 
 String lcd_status;
 
-unsigned long startMillis;
-unsigned long currentMillis;
+unsigned long startMillis, currentMillis;
 const unsigned long PERIOD = 100;  // milliseconds
 
-const bool ON = LOW;
-const bool OFF = HIGH;
-
+const bool ON = LOW, OFF = HIGH;
 const bool RUN_MOTORS = true; // change this to false for testing without motors
 
 String get_status(int Arm1Speed, int PlatterSpeed, int Arm2Speed) {
   return "A" + String(Arm1Speed) + " B" + PlatterSpeed + " C" + Arm2Speed + "   ";
 }
 
+void set_waveforms() {
+  Serial.println("Set waveforms (Y)?"); //Prompt User for Input
+  while (Serial.available() == 0); // Wait for User
+
+  String yes_no = Serial.readString(); //Read the data the user has input
+  if (yes_no.startsWith("Y")) {
+    Serial.println("Arm1: 0) None, 1) square, 2) saw, 3) triangle, 4) sine"); //Prompt User for Input
+    while (Serial.available() == 0); // Wait for User
+
+    int arm1_wave_choice = Serial.parseInt(); //Read the data the user has input
+    if (arm1_wave_choice >= 0 && arm1_wave_choice <= 4) {
+      Serial.println("Arm1 wave set to :" + String(arm1_wave_choice));
+      arm1_wave = arm1_wave_choice;
+
+      Serial.println("Arm1 period?"); //Prompt User for Input
+      while (Serial.available() == 0); // Wait for User
+
+      int arm1_period_choice = Serial.parseInt(); //Read the data the user has input
+      Serial.println(arm1_period_choice);
+      if (arm1_period_choice > 0 && arm1_period_choice <= 60) {
+        Serial.println("Arm1 period set to :" + String(arm1_period_choice));
+        arm1_period = arm1_period_choice;
+
+        Serial.println("Arm1 amplitude?"); //Prompt User for Input
+        while (Serial.available() == 0); // Wait for User
+        int arm1_amp_choice = Serial.parseInt(); //Read the data the user has input
+        if (arm1_amp_choice > 0 and arm1_amp_choice <=100){
+          Serial.println("Arm1 amplitude set to :" + String(arm1_amp_choice));
+          arm1_amp = arm1_amp_choice;
+        }
+      }
+    }
+  } else {
+    Serial.println("Waveforms not set"); //Prompt User for Input
+  }
+}
+
 void setup() {
   // LCD
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd_status = get_status(Arm1Speed, PlatterSpeed, Arm2Speed);
   lcd.setCursor(0, 0);
   lcd.print(lcd_status);
@@ -85,16 +112,17 @@ void setup() {
   pinMode(Arm1Pin, OUTPUT);
   pinMode(Arm2Pin, OUTPUT);
   pinMode(PlatterPin, OUTPUT);
-  Serial.begin(9600);
 
-  startMillis = millis();  //initial start time
+  Serial.begin(9600);
+  // startMillis = millis();  //initial start time
+
+  set_waveforms();
 }
 
 void loop() {
 
   start_button = digitalRead(START_BUTTON);
-  if (start_button == ON && motorsOn == true) {
-    // the motors are running and might need adjustment
+  if (start_button == ON && motorsOn == true) { // the motors are running and might need adjustment
     /*
        This is where we might adjust the speeds of
        Arm1Speed and Arm2Speed according to:
@@ -119,6 +147,12 @@ void loop() {
        sine:
            plt the sine of currentMillis - startMillis
     */
+    Serial.print(arm1_wave);
+    Serial.print(arm2_wave);
+    Serial.print(arm1_period);
+    Serial.print(arm2_period);
+    Serial.print(arm1_amp);
+    Serial.println(arm2_amp);
   }
 
   if (start_button == ON && motorsOn == false) { // start the motors
@@ -184,7 +218,7 @@ void loop() {
     }
 
     // advance Arms
-    if (button_3_state == ON) {
+    if (button_3_state == ON) { // Arm1
       int advance_speed = int(Arm1Speed / 2);
       if (advance_speed < MOTOR_MIN) advance_speed = MOTOR_MIN;
       analogWrite(Arm1Pin, advance_speed);
@@ -194,8 +228,8 @@ void loop() {
       analogWrite(Arm1Pin, 0);
     }
 
-    if (button_9_state == ON) {
-      int advance_speed = int(Arm1Speed / 2);
+    if (button_9_state == ON) { // Arm2
+      int advance_speed = int(Arm2Speed / 2);
       if (advance_speed < MOTOR_MIN) advance_speed = MOTOR_MIN;
       analogWrite(Arm2Pin, advance_speed);
       while (digitalRead(BUTTON_9) == ON) { // advance Arm2
